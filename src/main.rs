37c6f756376b87a6dee62bc95f03208b22b04b1c
demerara151 <demeraraonfire@gmail.com>
2023-01-04 19:32:23 +0900

    change loop function

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,10 @@ fn main() {
         result_count_3: 0,
     };
 
-    let mut count: u32 = 0;
-    while count < 100_000_000 {
+    for _ in 1..100_000_000 {
         wish.wish();
-        count += 1
     }
 
-    let rarity_5: f64 = ((wish.result_count_5 * 100) / count).into();
-    let rarity_4: f64 = ((wish.result_count_4 * 100) / count).into();
-
-    println!(
-        "You've got *5 x{}. It's rarity is {}%.",
-        wish.result_count_5, rarity_5
-    );
-    println!(
-        "You've got *4 x{}. It's rarity is {}%.",
-        wish.result_count_4, rarity_4
-    )
+    println!("You've got *5 x{}", wish.result_count_5);
+    println!("You've got *4 x{}", wish.result_count_4)
 }
